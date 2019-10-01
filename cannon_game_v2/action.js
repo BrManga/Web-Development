6f489c7c9ee978819ad77b6a2cdd92a4f8d16e97
Manga <44432225+BrManga@users.noreply.github.com>
2019-10-01 10:09:02 +0200

    Add files via upload

---
+++ cannon_game_v2/action.js
@@ -0,0 +1,69 @@
+// Selecting Cannon Ball
+var ball = document.getElementById("ball");
+
+//select FIRE button
+var btnStrt = document.getElementById("start");
+btnStrt.addEventListener("click", startInterval);
+//select RESET button
+var btnreset = document.getElementById("reset");
+btnreset.addEventListener("click", reset);
+//Select WAll
+var wall = document.getElementById("wall");
+//Select WALL Button
+var btnsetwall = document.getElementById("wallbtn");
+btnsetwall.addEventListener("click", setwallheight);
+
+//SETING Wall Height
+wallHeight = document.getElementById("wallinput").value;
+function setwallheight(wallHeight) {
+  wall.style.height = `${wallHeight}px`;
+}
+// Variables
+var newx = 0;
+var newy = 0;
+var xStart = 50;
+var yStart = 50;
+var firstSpeed = 100;
+var time = 0;
+var go;
+var angle = 45;
+var radangle = 0;
+var wallHeight = 0;
+
+//Start the game
+function startInterval() {
+  firstSpeed = document.getElementById("firstSpeed").value;
+  angle = document.getElementById("angle").value;
+  firstSpeed = Number(firstSpeed);
+  go = setInterval(startGame, 100);
+}
+
+function startGame() {
+  radangle = angle * (Math.PI / 180);
+  newx = xStart + firstSpeed * time * Math.cos(radangle);
+  newy =
+    yStart +
+    firstSpeed * time * Math.sin(radangle) -
+    time * 9.81 * Math.pow(time, 2);
+  changePos();
+  time = time + 0.1;
+}
+
+// Changing position of the Cannon Ball
+function changePos() {
+  ball.style.left = `${newx}px`;
+  ball.style.bottom = `${newy}px`;
+  var stopIt = +newy;
+  if (stopIt < 0) {
+    clearInterval(go);
+    console.log(newx, newy);
+  }
+}
+function reset() {
+  ball.style.left = `50px`;
+  ball.style.bottom = "50px";
+  clearInterval(go);
+  newx = 0;
+  newy = 0;
+  time = 0;
+}
